4cb805f7020a802b811795b37759bc73a8d0e6c8
David <pairing4good@gmail.com>
2024-05-27 07:49:50 -0500

    added unit test

---
+++ tests/App.test.jsx
@@ -0,0 +1,25 @@
+import { vi } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import App from '../src/App'
+
+vi.mock('aws-amplify/data', () => ({
+    generateClient: () => { return {
+        models: {
+            Todo: {
+                observeQuery: () => {
+                    return {subscribe: vi.fn()}
+                }
+            }
+        }
+    }}
+  }))
+
+describe('App', () => {
+  it('renders the App component', () => {
+    render(<App />)
+    
+    const heading = screen.getByRole('heading', { level: 1 })
+ 
+    expect(heading).toBeInTheDocument()
+  })
+})
